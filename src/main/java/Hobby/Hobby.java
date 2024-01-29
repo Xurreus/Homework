ab1fe00a2e5460b4b0f5468b6cbcce47b0a902aa
Xurreus <chrisgames513@gmail.com>
2024-01-29 20:10:02 +0200

    Initial commit

---
+++ src/main/java/Hobby/Hobby.java
@@ -0,0 +1,20 @@
+package Hobby;
+
+import java.util.Scanner;
+
+public class Hobby {
+
+    public static void main(String[] args) {
+       System.out.println(" What's your name");
+
+
+         Scanner scanner = new Scanner(System.in);
+
+            String userName = scanner.nextLine();
+
+
+        System.out.println("Hello " + userName +". Nice to see you!");
+
+
+    }
+}
